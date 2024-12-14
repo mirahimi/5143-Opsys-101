a0029258e4e325747d531500a8ed2b9d7ef10617
Sly Rahimi <mirahimi@users.noreply.github.com>
2024-12-13 20:30:27 -0600

    Update cpu_jobs.py

--- Assignments/P03/cpu_jobs.py
+++ Assignments/P03/cpu_jobs.py
@@ -1,34 +1,40 @@
 ### Handles API Interactions
+# put in running jobs and finished jobs in right panel
+# have output stream in lower panel
+# add color
 
 import requests
 import json 
 import os
+import random
 from rich import print
 
 def getConfig(client_id):
     return {
         "client_id": "brett",
-        "min_jobs": 10, # Generate 5 jobs
-        "max_jobs":10,  # Ensure exactly 5 jobs
-        "min_bursts": 5,  # Each job has at least 2 bursts
-        "max_bursts": 5,  # At most 3 bursts per job
-        "min_job_interval": 10,  # New jobs arrive quickly (1 clock tick)
+        "min_jobs": 10, # Generate minimum 10 jobs
+        "max_jobs":10,  # Generate maximum 10 jobs
+        "min_bursts": 5,  # Each job has at least 5 bursts
+        "max_bursts": 5,  # At most 5 bursts per job
+        "min_job_interval": 10,  # New jobs arrive at minimum after 10 clock ticks
         "max_job_interval": 12,  # Max interval between job arrivals
-        "burst_type_ratio": 0.8,  # Higher chance of CPU bursts (70%)
-        "min_cpu_burst_interval": 40,  # Short CPU bursts (3 ticks)
-        "max_cpu_burst_interval": 45,  # Max CPU burst duration (5 ticks)
+        "burst_type_ratio": 0.8,  # Higher chance of CPU bursts (80%)
+        "min_cpu_burst_interval": 3,  # Short CPU bursts (3 ticks)
+        "max_cpu_burst_interval": 5,  # Max CPU burst duration (5 ticks)
         "min_io_burst_interval": 2,  # Short IO bursts (2 ticks)
         "max_io_burst_interval": 4,  # Max IO burst duration (4 ticks)
         "min_ts_interval": 5,  # Short timeslice intervals for preemption testing
         "max_ts_interval": 10,  # Upper bound for timeslice intervals
-        "priority_levels": [3],  # 3 priority levels for clear preemption
+        "priority_levels": [3],  # 3 priority levels -> this value is overwritten by terminal argument; if none passed, default is 3 priority queues
     }
 
-def init(config):
+def init(config, seed=None):
     """
     This function will initialize the client and return the `client_id` and `session_id`
     """
-    route = f"http://profgriffin.com:8000/init"
+    if not seed:
+        seed = random.randint(0, 10000000)
+    route = f"http://profgriffin.com:8000/init?seed={seed}"
     r = requests.post(route,json=config)
     if r.status_code == 200:
         response = r.json()
@@ -93,6 +99,7 @@ if __name__ == '__main__':
 
     start_clock = response['start_clock']
     session_id = response['session_id']
+    # time_quantum = response['time_slice']
 
     clock = start_clock
 
